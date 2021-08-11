3c8ebf794a71bb657a7375d30a21ced7b2d58f2e
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-08-11 13:03:32 -0400

    hello-secrets-v3

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "freda-bucket-testing-${var.account-id}"
+  bucket = "freda-bucket-testing-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 
   tags = {
@@ -12,7 +12,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "account-id" {
-  type = string
-  description = "the account id"
-}
+# variable "account-id" {
+#   type = string
+#   description = "the account id"
+# }
+
+
+
+data "aws_caller_identity" "current" {}
+# data.aws_caller_identity.current.account_id

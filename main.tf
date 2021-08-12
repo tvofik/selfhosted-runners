31a9ab050ee0465703d3644af84399f76389a7de
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2021-08-12 08:24:51 -0400

    Testing123

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "bucket" {
-  bucket = "freda-bucket-testing-${var.account-id}"
+  bucket = "freda-bucket-testing-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 
   tags = {
@@ -13,10 +13,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "account-id" {
-  type        = string
-  description = "the account id"
-}
+# variable "account-id" {
+#   type        = string
+#   description = "the account id"
+# }
 
 
 
